9a59c12588fdbca04cfbfb02792a24eaf3d0b37f
Yunitrish <yunthomas006@gmail.com>
2023-02-15 20:59:14 +0800

    update

--- RRI/example_code.py
+++ RRI/example_code.py
@@ -1,17 +1,27 @@
-from machine import Pin, PWM
+#import the needed modules
+from network import WLAN
+import machine
 import time
 
-ir_pin = Pin(14, Pin.IN)
-pwm_pin = PWM(Pin(12))
+#initialize the infrared remote control 
+ir = machine.Pin(4, machine.Pin.IN)
 
-pwm_pin.freq(40)
+#set up the ESP32 as an access point
+wlan = WLAN(mode=WLAN.AP)
+wlan.antenna(WLAN.INT_ANT)
+wlan.ifconfig(config=('192.168.4.1', '255.255.255.0', '192.168.4.1', '8.8.8.8')) 
+wlan.init(mode=WLAN.AP, ssid='esp32', auth=(WLAN.WPA2, 'esp32password'), channel=7, antenna=WLAN.INT_ANT) 
 
-while ir_pin.value() == 0:
-    pass
-
-pwm_pin.duty(512)
-
-while ir_pin.value() == 1:
-    pass
-
-pwm_pin.duty(0)
+#infinite loop to capture infrared remote control signals and act accordingly
+while True:
+    remote_control_id = ir.value()  #capture the signal from infrared remote control
+    if remote_control_id == 1: #if signal corresponding to id 1 is received
+        #connect to WI-FI and save the credentials 
+        wlan = WLAN(mode=WLAN.STA) 
+        wlan.connect('SSID', auth=(WLAN.WPA2, 'password')) 
+        wlan.config(essid='SSID', authmode=WLAN.WPA2, password='password') 
+        wlan.save()
+        time.sleep(5) #wait for 5 seconds before proceeding to next statement 
+    elif remote_control_id == 2: #if signal corresponding to id 2 is received 
+        #disconnect from WI-FI
+        wlan.disconnect()
