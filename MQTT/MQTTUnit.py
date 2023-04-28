90aaf3c85ff50299dec1e95038882c5586a4a95d
Yunitrish <yunthomas006@gmail.com>
2023-04-28 10:57:30 +0800

    update

--- MQTT/MQTTUnit.py
+++ MQTT/MQTTUnit.py
@@ -3,22 +3,18 @@ from re import T
 import paho.mqtt.client as mqtt
 import threading
 
-class Dosomething:
+class MQTTUnit:
     # 設定MQTT代理和主題
     topic = "NCUEMQTT"
     client = mqtt.Client()
     thread_list = []
-    # 設定連接和訊息處理程序
-    def on_connect(self,client, userdata, flags, rc) -> None:
-        # print("Connected with result code "+str(rc))
-        self.client.subscribe(self.topic)
     # 處理收到的訊息
     def on_message(self,client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
     def __init__(self):
         self.thread_list = []
         # 連接到MQTT代理並啟動訊息處理程序
-        self.client.on_connect = self.on_connect
+        self.client.on_connect = self.client.subscribe(self.topic)
         self.client.on_message = self.on_message
         self.client.connect("test.mosquitto.org", 1883, 60)
     
@@ -37,5 +33,5 @@ class Dosomething:
             i.join()
 
 if __name__ == "__main__":
-    runner = Dosomething()
+    runner = MQTTUnit()
     runner.run()
