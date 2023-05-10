11fc49fbc9d5d45d0235374e773c4fbd30b95178
Yunitrish <yunthomas006@gmail.com>
2023-05-10 22:47:22 +0800

    update

--- MQTT/test2.py
+++ MQTT/test2.py
@@ -4,36 +4,37 @@ from umqtt.simple import MQTTClient
 from machine import Pin
 import dht
 
-dht11 = dht.DHT11(Pin(14)) 
+def main():
+    dht11 = dht.DHT11(Pin(14))
 
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('V2041', '123456789')
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('V2041', '123456789')
 
-while not sta_if.isconnected():
-    pass
-print("connected")
+    while not sta_if.isconnected():
+        pass
+    print("connected")
 
-client = MQTTClient(
-    client_id="client", 
-    keepalive=5,
-    server="test.mosquitto.org", 
-    ssl=False)
+    client = MQTTClient(
+        client_id="client",
+        keepalive=5,
+        server="test.mosquitto.org",
+        ssl=False)
 
-client.connect()
+    client.connect()
 
-def get_msg(topic, msg):
-    print(msg)
+    def get_msg(topic, msg):
+        print(msg)
 
-client.set_callback(get_msg)
-client.subscribe("MQTT")
+    client.set_callback(get_msg)
+    client.subscribe("MQTT")
 
-counter = 0
-while True:
-    client.check_msg()
-    dht11.measure()
-    string = "T:" + str(dht11.temperature()) + " H:" + str(dht11.humidity())
-    print(string)
-    client.publish("MQTT", string)
-    counter = counter + 1
-    time.sleep(1)
+    counter = 0
+    while True:
+        client.check_msg()
+        dht11.measure()
+        string = "T:" + str(dht11.temperature()) + " H:" + str(dht11.humidity())
+        print(string)
+        client.publish("MQTT", string)
+        counter = counter + 1
+        time.sleep(1)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from MQTT.unit import Unit
-
+# from MQTT.unit import Unit
+from MQTT import test2
 
 def main():
-    runner = Unit(input("user name: "))
-    runner.run()
+    print("launching...")
+    test2.main()
+    # runner = Unit(input("user name: "))
+    # runner.run()
 
 
 main()
