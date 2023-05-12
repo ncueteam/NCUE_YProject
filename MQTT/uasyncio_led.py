5715ab8e21f17fcce538276ca8071eafe68ccaba
Yunitrish <yunthomas006@gmail.com>
2023-05-12 11:17:16 +0800

    mqtt uasyncio

--- MQTT/uasyncio_led.py
+++ MQTT/uasyncio_led.py
@@ -1,9 +1,43 @@
 import uasyncio as asyncio
 import machine
 import network
+from umqtt.simple import MQTTClient as Client
 
 SSIDs = ["Yunitrish", "603"]
 PWDs = ["0937565253", "0937565253"]
+
+
+class Unit:
+    topic = "NCUEMQTT"
+    port = 1883
+    thread_list = []
+    username = ""
+    received_count = 0
+    server = "test.mosquitto.org"
+    client = Client(client_id=username, server=server)
+
+    def on_connect(self, client, userdata, flags, rc) -> None:
+        # print("Connected with result code "+str(rc))
+        self.client.subscribe(self.topic)
+
+    def on_message(self, userdata, msg):
+        self.received_count += 1
+        print(msg.topic + " " + str(msg.payload))
+
+    def __init__(self, username: str):
+        self.thread_list = []
+        self.username = username
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.topic = self.topic
+        self.client.port = self.port
+        self.client.server = self.server
+        self.client.ssl = False
+        self.client.connect()
+
+    def send_message(self) -> None:
+        self.client.publish(self.topic, "[" + self.username + "]\t" + input())
+        # self.send_message()
 
 
 async def connect_net(ssid: list[str], password: list[str], num: int):
@@ -15,10 +49,10 @@ async def connect_net(ssid: list[str], password: list[str], num: int):
         print("Connected to network:", wlan.ifconfig()[0])
     except KeyboardInterrupt:
         print("Connection interrupted by user.")
-        await connect_net(ssid, password, num-1)
+        await connect_net(ssid, password, num - 1)
     except Exception as e:
         print("Error connecting to network:", e)
-        await connect_net(ssid, password, num-1)
+        await connect_net(ssid, password, num - 1)
 
 
 async def blink(led_pin, gap: int, time: int):
@@ -43,7 +77,13 @@ async def main():
     led_pin = machine.Pin(2, machine.Pin.OUT)
     await asyncio.create_task(blink(led_pin, 100, 10))
     await connect_net(SSIDs, PWDs, 0)
-    await asyncio.create_task(blink(led_pin, 1500, -1))
+    runner = Unit(input("username: "))
+
+    async def loopor():
+        runner.send_message()
+        await asyncio.create_task(blink(led_pin, 1500, 1))
+        await loopor()
+    await loopor()
 
 
 def run():
