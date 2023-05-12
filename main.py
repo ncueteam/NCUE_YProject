6e4a530dd9de1929aa92070facf6b1a017309923
Yunitrish <yunthomas006@gmail.com>
2023-05-12 10:41:38 +0800

    uasyncio_led.py update

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# from MQTT.unit import Unit
 from MQTT import uasyncio_led
-import uasyncio
+
+
 def main():
     print("launching...")
     uasyncio_led.run()
-    # runner = Unit(input("user name: "))
-    # runner.run()
 
 
 main()
-
