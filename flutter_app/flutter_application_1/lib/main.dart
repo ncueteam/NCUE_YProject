ffaa0f95dabdb99d8234d0d73a3059720266a740
yan-xuan Li <tsing98712372@yahoo.com>
2023-05-17 20:54:57 +0800

    file access

--- flutter_app/flutter_application_1/lib/main.dart
+++ flutter_app/flutter_application_1/lib/main.dart
@@ -46,20 +46,20 @@ void main() {
 //import 'dart:async';
 //import 'dart:html';
 
-import 'dart:html';
+//import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:shelf/shelf.dart' as shelf;
+//import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_static/shelf_static.dart';
 
-final staticHandler = createStaticHandler('path_to_your_files',
-    defaultDocument: 'assets/Profile.vue');
-final port = 8080; // Choose a port for the local server
-final server = shelf_io.serve(staticHandler, 'localhost', port);
+
 void main() {
+  final staticHandler = createStaticHandler('path_to_your_files', defaultDocument: 'assets/Profile.vue');
+  final port = 8080; // Choose a port for the local server
+  final server = shelf_io.serve(staticHandler, 'localhost', port);
   runApp(
     MaterialApp(
       theme: ThemeData(useMaterial3: true),
@@ -91,7 +91,7 @@ class _WebViewAppState extends State<WebViewApp> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(
-        Uri.parse('http://localhost:8080'),
+        Uri.parse('http://localhost:8080/'),
         //'assets/Profile.vue'
         //'assets/vue-soft-ui-dashboard-main/src/views/Profile.vue'
       )
