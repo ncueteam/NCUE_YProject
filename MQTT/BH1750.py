96050efbdc7cee444c65d5993ed701567037c08b
kirkl <kirklan0423@gmail.com>
2023-05-23 23:43:17 +0800

    BH1750

--- MQTT/BH1750.py
+++ MQTT/BH1750.py
@@ -1,9 +1,7 @@
-# GY_30光照强度传感器
 import time
 from machine import I2C, Pin
 import network
 from umqtt.simple import MQTTClient
-
 
 #VCC接3.3V
 #GND接CND
@@ -11,49 +9,53 @@ from umqtt.simple import MQTTClient
 #SDA接P4
 #ADDR接P19
 
-def main():
-    i2c = I2C(scl=Pin(15), sda=Pin(4), freq=10000)  # 软件I2C
-    addr_list = i2c.scan()
-    print('addr_list:', addr_list)
 
-    i2c.writeto(addr_list[0], b'\x01')# BH1750通电，进入等待测量状态
-    i2c.writeto(addr_list[0], b'\x10')# 设置分辨率模式为连续 H分辨率模式
+i2c = I2C(scl=Pin(15), sda=Pin(4), freq=10000)  # 软件I2C
+addr_list = i2c.scan()
+print('addr_list:', addr_list)
 
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect('V2041', '123456789')
+i2c.writeto(addr_list[0], b'\x01')# BH1750通电，进入等待测量状态
+i2c.writeto(addr_list[0], b'\x10')# 设置分辨率模式为连续 H分辨率模式
 
-    while not sta_if.isconnected():
-        pass
-    print("connected")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('V2041', '123456789')
 
-    client = MQTTClient(
-        client_id="client",
-        keepalive=5,
-        server="test.mosquitto.org",
-        ssl=False)
+while not sta_if.isconnected():
+    pass
+print("connected")
 
-    client.connect()
+client = MQTTClient(
+    client_id="client",
+    keepalive=5,
+    server="test.mosquitto.org",
+    ssl=False)
 
-    def get_msg(topic, msg):
-        print(msg)
+client.connect()
 
-    client.set_callback(get_msg)
-    client.subscribe("MQTT")
+def get_msg(topic, msg):
+    print(msg)
 
+client.set_callback(get_msg)
+client.subscribe("MQTT")
 
-    def GY_30_sensor():
-        # 读取测量结果
-        data = i2c.readfrom(35, 2)
-        # 处理测量结果
-        result = float(data[0] * 0xff + data[1]) / 1.2
-        # 将测量结果返回
-        return result
+def GY_30_sensor():
+    data = i2c.readfrom(35, 2)# 读取测量结果
+    result = float(data[0] * 0xff + data[1]) / 1.2 # 处理测量结果
+    time.sleep_ms(2000)
+    return result# 将测量结果返回
 
-    counter = 0
-    while True:
-        string ="光照值 = " + GY_30_sensor()
-        print(string)
-        client.publish("MQTT", string)
-        time.sleep_ms(2000)
+counter = 0
+while True:
+    client.check_msg()
+    #data = GY_30_sensor()
+    #string ="光照值 = " + str(GY_30_sensor())
+    data = i2c.readfrom(35, 2)# 读取测量结果
+    result = float(data[0] * 0xff + data[1]) / 1.2 # 处理测量结果
+    string ="L= " + str(result)
+    print(string)
+    client.publish("MQTT", string)
+    counter = counter + 1
+    time.sleep(1)
+
 
