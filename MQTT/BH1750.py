5cbb677a1dcb4f063a7655832eb6ba22cd0f1493
kirkl <kirklan0423@gmail.com>
2023-05-23 23:08:27 +0800

    BH1750

--- MQTT/BH1750.py
+++ MQTT/BH1750.py
@@ -1,19 +1,35 @@
-from machine import Pin,I2C
+# GY_30光照强度传感器
 import time
+from machine import I2C, Pin
 
-i2c = I2C(0,scl = Pin(15),sda = Pin(4),freq = 1_000_000)
-scan = i2c.scan()
-print(scan)
-#print(hex(i2c.scan()[0]),hex(i2c.scan()[1]))  # 打印器件I2C地址
-BH1750_I2C_ADD  = 0x23
+i2c = I2C(scl=Pin(15), sda=Pin(4), freq=10000)  # 软件I2C
+addr_list = i2c.scan()
+print('addr_list:', addr_list)
+# result = bh1750fvi.sample(i2c) # in lux
+# print(result)
 
-#i2c.writeto(BH1750_I2C_ADD,chr(0x01)) # 通电运行
-#i2c.writeto(BH1750_I2C_ADD,chr(0x07)) # 复位
-#i2c.writeto(BH1750_I2C_ADD,chr(0x10)) # 横向分辨率连续读取 1 Lx 120ms
+# BH1750通电，进入等待测量状态
+i2c.writeto(addr_list[0], b'\x01')
+
+# 设置分辨率模式为连续 H分辨率模式
+i2c.writeto(addr_list[0], b'\x10')
+
+#VCC接3.3V
+#GND接CND
+#SCL接P15
+#SDA接P4
+#ADDR接P19
+
+def GY_30_sensor():
+    # 读取测量结果
+    data = i2c.readfrom(35, 2)
+    # 处理测量结果
+    result = float(data[0] * 0xff + data[1]) / 1.2
+    # 将测量结果返回
+    return result
+
 
 while True:
-    gy = 0
-    gy = i2c.readfrom(BH1750_I2C_ADD,2) #0-65535 1 8bit 2  int 16 char 8
-    gy30 = float(gy[0] << 8 | gy[1])/1.2 #左移动，可以理解为乘法 gy[0]*0xff
+    print(GY_30_sensor())
     time.sleep_ms(2000)
-    print("光照值 = %.2f Lx" %gy30)
+
