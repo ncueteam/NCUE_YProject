4f86c4120436efdc6262157d978728ad629e10e3
Yunitrish <yunthomas006@gmail.com>
2023-07-16 13:48:12 +0800

    2023/07/16

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import uasyncio
-from device.segment_7 import displayor
+from lib.device.segment_7 import displayor
+
+from lib.file_set import FileSet
+
+fileset = FileSet(folder='database',file_name='wifi')
+fileset.initialize()
+fileset.add("302", "0937565253")
+fileset.add("V2041", "123456789")
+
 
 loop = uasyncio.get_event_loop()
 loop.run_until_complete(displayor.main())
