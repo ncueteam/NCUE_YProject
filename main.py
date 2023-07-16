70ad56ea5d3e8981ca78dbd9bf0f255ecbf45567
Yunitrish <yunthomas006@gmail.com>
2023-07-16 13:58:39 +0800

    2023/07/16 .1

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from lib.device.segment_7 import displayor
 
 from lib.file_set import FileSet
 
-fileset = FileSet(folder='database',file_name='wifi')
-fileset.initialize()
+fileset = FileSet(folder='database', file_name='wifi')
 fileset.add("302", "0937565253")
 fileset.add("V2041", "123456789")
 
